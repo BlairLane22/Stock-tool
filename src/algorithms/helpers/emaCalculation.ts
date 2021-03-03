c1a40430c192b565e975ee5d6287918cbd6a0065
Blair Lane <blairjacklane@gmail.com>
2021-03-02 19:56:51 -0500

    Adding the emaCalculation and macd

---
+++ src/algorithms/helpers/emaCalculation.ts
@@ -0,0 +1,27 @@
+interface Candle {
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  timeStamp: number;
+}
+
+function emaMovingAverage(days: number, candles: Candle[]): number {
+  let ema = 0;
+  let ema_before = 0;
+  const smoothing_factor = 2 / (days + 1);
+  const multiplier = smoothing_factor / (1 + days);
+
+  for (let a = 0; a < days; a++) {
+    const x = 1 - multiplier;
+    const y = candles[a].close;
+
+    ema = y + ema_before * x;
+    ema_before = ema;
+  }
+
+  return ema;
+}
+
+export { emaMovingAverage };
