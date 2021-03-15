257a39ed88149445b14de2e1fb1b408856a9b5f3
Blair Lane <blairjacklane@gmail.com>
2021-03-15 14:18:08 -0400

    Commit

--- src/algorithms/macd.ts
+++ src/algorithms/macd.ts
@@ -22,9 +22,9 @@ function macdBuyDecision(quote: Quote, candles: Candle[]): boolean {
   const ema_26 = emaCalculation(30, 350, quote, candles);
   const ema_27 = emaCalculation(60, 350, quote, candles);
 
-  console.log(ema_12);
-  console.log(ema_26);
-  console.log(ema_27);
+  // console.log(ema_12);
+  // console.log(ema_26);
+  // console.log(ema_27);
 
   const ema_final = ema_12[9] - ema_26[25];
   if (ema_final > 0) {
